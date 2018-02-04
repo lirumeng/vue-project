47505d510f6137d9ad4ce1a0820646d11684bcc8
111 <a111@192.168.2.104>
2018-02-04 21:33:29 +0800

    ui completed

---
+++ src/todo/todo.vue
@@ -0,0 +1,62 @@
+<template>
+	<section class="real-app">
+		<input 
+			type="text"
+			class="add-input"
+			autofocus="autofocus"
+			placeholder="接下来要做什么？"
+			@keyup.enter="addTodo">
+		<item :todo="todo"></item>
+		<tabs :filter="filter"></tabs>
+	</section>
+</template>
+
+<script>
+import Item from './item.vue'
+import Tabs from './tabs.vue'
+
+export default {
+	data(){
+		return {
+			todo: {
+				id: 0,
+				content: 'this is todo',
+				completed: false
+			},
+			filter: 'all'
+		}
+	},
+	components: {
+		Item,
+		Tabs,
+	},
+	methods: {
+		addTodo() {}
+	}
+}
+
+</script>
+
+<style lang="stylus" scoped>
+.real-app{
+	width 600px
+	margin 0 auto
+	box-shadow 0 0 5px #666
+}
+.add-input{
+	position relative
+	margin 0
+	width 100%
+	font-size 24px
+	font-family inherit
+	font-weight inherit
+	line-height 1.4em
+	outline none
+	color inherit
+	box-sizing border-box
+	-webkit-font-smoothing antialiased
+	padding 16px 16px 16px 60px
+	border none
+	box-shadow inset 0 -2px 1px rgba(0,0,0,.7)
+}
+</style>
